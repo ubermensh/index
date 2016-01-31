9c09255b05d005768d9009f6c8d18e71b0d51597
geshtalt <lemantar.flcl@gmail.com>
2016-01-31 18:46:40 +0700

    basic data processing

--- src/server.js
+++ src/server.js
@@ -1,20 +1,43 @@
 var http = require('http');
-var path = require('path');
 var fs = require('fs');
-var formidable = require('formidable')
+var formidable = require('formidable');
+var path = require('path');
+var util = require('util');
+
 var server = http.createServer(function (req, res) {
-    displayForm(res);
+    if (req.method.toLowerCase() == 'get') {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+
 });
 
 function displayForm(res) {
     fs.readFile(path.resolve(__dirname, 'form.html'), function (err, data) {
-	if(err) throw err;  
+	if(err) throw err;
         res.writeHead(200, {
             'Content-Type': 'text/html',
                 'Content-Length': data.length
         });
         res.write(data);
         res.end();
+    });
+}
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields ) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields
+        }));
     });
 }
 
