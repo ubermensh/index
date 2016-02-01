7ee6b5ead89d69f6a382448b4abf6b2fa90195fb
geshtalt <lemantar.flcl@gmail.com>
2016-02-01 17:20:44 +0700

    bootstrap used

--- src/modules/bmiCalculator.js
+++ src/modules/bmiCalculator.js
@@ -1,5 +1,6 @@
 module.exports = function (height, weight) {
     //sm to meters
     height = height / 100;
-    return weight / (height * height);
+    bmi =  weight / (height * height);
+    return Math.round( bmi * 10 ) / 10;
 };
