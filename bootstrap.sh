59eb20eb281e7025f238c1f35625f972a35de99d
geshtalt <lemantar.flcl@gmail.com>
2016-02-02 09:41:06 +0700

    provision file: removed local npm package installing, added check if package installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
-set -o vi
 
 echo "--- Updating packages list 1 ---"
 sudo apt-get -qq update
 
-echo "--- Installing base packages ---"
-sudo apt-get -qq install -y vim git curl python-software-properties
+echo "--- Installing base packages (if not installed) ---"
+
+package_installed () {
+    type "$1" &> /dev/null ;
+}
+
+for package in vim git curl python-software-properties
+do
+    if ! package_installed $package; then
+        echo install $package
+        sudo apt-get -qq install -y $package
+    else
+        echo  $package already installed
+
+    fi
+done
+
+if ! package_installed node; then
+     echo "--- Install node.js/npm using n--"
+    n_directory='/opt/n'
+    git clone https://github.com/visionmedia/n.git $n_directory
+    cd $n_directory
+    make install
+    n stable
+fi
 
 echo "--- Updating packages list 2 ---"
 sudo apt-get -qq update
 
-echo "--- install n ---"
-n_directory='/opt/n'
-git clone https://github.com/visionmedia/n.git $n_directory
-cd $n_directory
-make install
-
-echo "--- Install node.js/npm using n--"
-n stable 
-
-echo "--- Install the local dependencies ---"
-cd /vagrant
-npm install --save-dev  grunt-cli bower selenium-webdriver mocha chai
-npm install --save-dev formidable@latest express jade
-npm install --save-dev nodemon babel-polyfill
+echo " provisioning done "
 
